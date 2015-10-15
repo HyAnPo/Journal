0669db88892a170152567af821f8ee7764895a98
Andrew Porter <haporter@me.com>
2015-10-15 17:02:49 -0600

    Complete to step 5

---
+++ EntryController.swift
@@ -0,0 +1,36 @@
+//
+//  EntryController.swift
+//  Journal
+//
+//  Created by Andrew Porter on 10/15/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+
+class EntryController {
+    
+    //Add entries Array property, set it to empty in the initializer
+    // TODO: is our singleton correct?
+    static let singleton = EntryController()
+    
+    // Create empty entries array
+    var entriesArray: [Entry] = []
+    
+    // Add entry function
+    func addEntry(entry: Entry) -> () {
+        entriesArray.append(entry)
+    }
+    
+    //Remove entry function
+    func removeEntry(specificEntry: Entry) -> () {
+        let index = self.entriesArray.indexOf(specificEntry)
+        
+        // This finds a index and then removes the entry at that index.
+        if let entryIndex = index {
+            self.entriesArray.removeAtIndex(entryIndex)
+        }
+    }
+    
+}
---
+++ Journal/Entry.swift
@@ -0,0 +1,26 @@
+//
+//  Entry.swift
+//  Journal
+//
+//  Created by Andrew Porter on 10/15/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class Entry: Equatable {
+    var timestamp: NSDate
+    var title: String
+    var bodyText: String
+    
+    // TODO: Not sure if the timestamp defaut is set right.
+    init(title: String, bodyText: String) {
+        self.timestamp = NSDate()
+        self.title = title
+        self.bodyText = bodyText
+    }
+}
+
+func ==(lhs: Entry, rhs: Entry) -> Bool {
+    return true
+}
